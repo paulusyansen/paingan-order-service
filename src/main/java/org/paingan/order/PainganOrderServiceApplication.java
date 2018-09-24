3a86d7bca8bf31807e342693af6bfeda9bc96033
paulusyansen <newcurzon@yahoo.com>
2018-09-24 18:08:44 +0700

    update config

--- src/main/java/org/paingan/order/PainganOrderServiceApplication.java
+++ src/main/java/org/paingan/order/PainganOrderServiceApplication.java
@@ -2,14 +2,14 @@ package org.paingan.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import brave.sampler.Sampler;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients("org.paingan.order.client")
 public class PainganOrderServiceApplication {
 
